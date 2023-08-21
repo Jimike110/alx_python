56377e91cc5c5779939f7b9d1df2e99e0c1f681d
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-21 16:22:25 +0000

    Task 3. The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10000, 10000)
-last_digit = str(number)[-1]
-if int(last_digit) > 5 and number > 0:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif int(last_digit) == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif int(last_digit) != 0 and number < 0:
-    last_digit = -int(last_digit)
-    if int(last_digit) < 6:
-        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+number = -98
+
+# Get the last digit of the number
+last_digit = abs(number) % 10  # Extract the last digit using modulo
+
+# Apply the original sign to the last digit
+if number < 0:
+    last_digit *= -1
+
+# Determine the appropriate message based on the last digit
+if last_digit > 5:
+    message = "and is greater than 5"
+elif last_digit == 0:
+    message = "and is 0"
+else:
+    message = "and is less than 6 and not 0"
+
+# Print the result
+print("The string Last digit of", number, "is", last_digit, message)
