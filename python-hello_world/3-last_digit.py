5d59e867e4239d85fd32cba8a6fac62c26b0903d
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-21 16:12:13 +0000

    Task 3. The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,5 +6,7 @@ if int(last_digit) > 5 and number > 0:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif int(last_digit) == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
-elif int(last_digit) < 6 and int(last_digit) != 0 and number < 0:
-    print(f"Last digit of {number} is -{last_digit} and is less than 6 and not 0")
+elif int(last_digit) != 0 and number < 0:
+    last_digit = -int(last_digit)
+    if int(last_digit) < 6:
+        print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
