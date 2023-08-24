e3385b76982a82eb066eff84c10443940dde6808
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-24 19:33:43 +0000

    Added an edge case if number < 2 or negative

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -5,7 +5,10 @@ def is_prime(number):
         if number % i == 0:
             prime = False
             break
-    if prime:
+    if number <= 1:
+        prime = False
+    elif prime:
         return True
     else:
         return False
+is_prime(22)
