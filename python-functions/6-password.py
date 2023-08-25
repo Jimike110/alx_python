96cef2b91488d8ebe4d19b80757f7ee9032d83f0
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-25 14:28:42 +0000

    Task 6. Password Validation Function

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -9,3 +9,27 @@
 # Returns True if the password passes all the checks, and False otherwise.
 
 # You are not allowed to import any module.
+
+def validate_password(password):
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+    has_space = False
+    
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+        elif char.isspace():
+            has_space = True
+            
+    # Check if all conditions are met
+    if has_uppercase and has_lowercase and has_digit and len(password) >= 8 and not has_space:
+        print(password, "fits.")
+        return True
+    else:
+        print(password, "does not fit.")
+        return False
