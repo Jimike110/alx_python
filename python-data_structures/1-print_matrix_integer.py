4e0ab7a012b4a9aee807b25af365dccdb4f00086
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-26 23:47:14 +0000

    Task 1. Lists of lists = Matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,32 @@
+# Write a function that prints a matrix of integers.
+# Prototype: def print_matrix_integer(matrix=[[]]):
+# Format: see example
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
+# You are not allowed to import any module
+# You can assume that the list only contains integers
+# You are not allowed to cast integers into strings
+# You have to use str.format() to print integers
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            # Print the element without moving to the next line
+            print("{}".format(matrix[i][j]), end=" ")
+        print()  # Move to the next line after each row
+    if matrix is not list:
+        print(matrix)
+
+
+# Define the matrix
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# Call the function with the defined matrix
+print_matrix_integer(matrix)
