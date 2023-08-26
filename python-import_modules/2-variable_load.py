8843785cc01982a82c607950bc32d4be39e199b7
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-26 01:17:35 +0000

    Task 2. Everything can be imported

---
+++ python-import_modules/2-variable_load.py
@@ -0,0 +1,8 @@
+# Write a program that imports the variable a from the file variable_load_2.py and prints its value.
+# You are not allowed to use {removed} for importing or {removed}
+# Your code should not be executed when imported
+
+if __name__ == "__main__":
+    from variable_load_2 import a
+
+    print(a)
