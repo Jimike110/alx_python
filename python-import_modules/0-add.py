83a0ebdc1eda37c42bea5176073164ae8224b51c
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-26 00:04:45 +0000

    Removed other instances of add_0 in comments

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,4 @@
-# Write a program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
+# Write a program that imports the function def add(a, b): from the file {removed} and prints the result of the addition 1 + 2 = 3
 # You have to use print function with string format to display integers
 # You have to assign:
 # the value 1 to a variable called a
@@ -6,7 +6,7 @@
 # and use those two variables as arguments when calling the functions add and print
 # a and b must be defined in 2 different lines: a = 1 and another b = 2
 # Your program should print: <a value> + <b value> = <add(a, b) value> followed with a new line
-# You can only use the word add_0 once in your code
+# You can only use the word {removed} once in your code
 # You are not allowed to use * for importing or __import__
 # Your code should not be executed when imported - by using __import__
 
