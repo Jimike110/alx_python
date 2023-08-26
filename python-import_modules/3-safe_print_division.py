1af07997c21ec34ef70a9354eebd356059367064
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-26 11:28:24 +0000

    Task 3. Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -18,6 +18,3 @@ def safe_print_division(a, b):
             print("Inside result: {}".format(result))
         else:
             print("Inside result = None")
-        return result
-
-safe_print_division(10, 0)
