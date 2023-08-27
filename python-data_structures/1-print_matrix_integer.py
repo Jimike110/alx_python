9c835497f2d396552b2c6812b8a50be0e4ed0083
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-27 00:08:11 +0000

    Task 1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -17,4 +17,4 @@ def print_matrix_integer(matrix=[[]]):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end=" " if j < len(matrix[i]) - 1 else "\n")
     if len(matrix[i]) == 0:
-        print(" ")
+        print("")
