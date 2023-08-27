583c27c47e8d7ca10f400e9e1dbb34d290d14bea
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-27 00:03:27 +0000

    Task 1. Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -11,10 +11,8 @@
 # You are not allowed to cast integers into strings
 # You have to use str.format() to print integers
 
+
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            # Print the element without moving to the next line
-            print("{:d}".format(matrix[i][j]), end=" ")
-        print()  # Move to the next line after each row
-
+            print("{:d}".format(matrix[i][j]), end=" " if j < len(matrix[i]) - 1 else "\n")
