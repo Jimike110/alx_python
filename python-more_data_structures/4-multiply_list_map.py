8d273c39edfa21f066e549fa0f92023fe104584e
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-31 18:54:55 +0000

    4. Multiply by using map

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,14 +1,3 @@
-# Write a function that returns a list with all values multiplied by a number without using any loops.
-# Prototype: def multiply_list_map(my_list=[], number=0):
-# Returns a new list:
-# Same length as my_list
-# Each value should be multiplied by number
-# Initial list should not be modified
-# You are not allowed to import any module
-# You have to use map
-# Your file should be max 3 lines
-
-
+# "Your file should be max 3 lines" - reason why this doesn't include instructions. Sorry.
 def multiply_list_map(my_list=[], number = 0):
     return list(map(lambda x: x * number, my_list))
-
