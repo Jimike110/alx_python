8d5e0ec2eb65eed51a5ed875b28a047b8ac13592
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-08-31 17:19:29 +0000

    Task 2. Update dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -7,7 +7,6 @@
 # You are not allowed to import any module
 
 
-def update_dictionary(a_dictionary, key=str, value=any):
-    a_dictionary = {}
+def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
