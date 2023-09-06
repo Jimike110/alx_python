5fad57f98a044e27061c9d0778cd53762e4646d7
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-06 08:28:43 +0100

    Task 0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,14 +2,38 @@
 # Private instance attribute: size
 # Instantiation with size (no type/value verification)
 # You are not allowed to import any module
+
 """
-Module is documented.
+This is the module docstring.
+It provides an overview of what this module contains.
 """
+
 class Square:
     """
-    Class is documented. Lol.
-    """
-    __size = None
+    This class defines a square.
 
-    def __init__(self, __size):
-        size = __size
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the given size.
+        area(self): Calculates and returns the area of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
