5edf6b16f970d402a9e5149802ab1d4a1585977a
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-06 08:39:17 +0100

    Task 1. Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,19 +6,44 @@
 # You are not allowed to import any module
 
 """
-This is the module documentation.
+This is the module docstring.
+It provides an overview of what this module contains.
 """
 
 class Square:
     """
-    Class documentation
+    This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the given size.
+        area(self): Calculates and returns the area of the square.
     """
-    def __init__(self, size=0):
-        __size = size
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
 
         try:
             int(size)
         except TypeError:
             print("size must be an integer")
+        
         if size < 0:
             raise ValueError(print("size must be >= 0"))
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
