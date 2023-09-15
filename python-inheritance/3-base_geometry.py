5b9dd7283a73b0e5efb1c8064dd409d296f5e397
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-15 22:45:16 +0100

    Task 3. Geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,19 @@
+# Write an empty class BaseGeometry.
+# You are not allowed to import any module
+
+"""
+This is the module docstring.
+It provides an overview of what this module contains.
+"""
+
+class BaseGeometry:
+    """
+    This is the BaseGeometry class.
+    
+    Attributes:
+        (None)
+    
+    Methods:
+        (None)
+    """
+    pass
