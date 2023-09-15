ed93043f3cdf6699cde93189a1468680ed9373a6
Michael Oladoye <108640730+Jimike110@users.noreply.github.com>
2023-09-15 07:34:07 +0100

    Added module and function documentations

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -2,5 +2,21 @@
 # Prototype: def is_kind_of_class(obj, a_class):
 # You are not allowed to import any module
 
+"""
+This is the module docstring.
+It provides an overview of what this module contains.
+"""
+
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class) or issubclass(obj, a_class)
+    """
+    Check if an object is an instance of, or if the object is an instance of a class that inherited from,
+    the specified class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to compare the object with.
+
+    Returns:
+        bool: True if the object is an instance of, or inherited from, the specified class; otherwise, False.
+    """
+    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
