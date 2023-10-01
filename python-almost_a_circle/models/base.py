6621fc6d88824f222c5ad29c1cd9c37c802b6471
Jimike <oladoyemike@gmail.com>
2023-10-01 22:13:53 +0100

    Task 0. Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -11,10 +11,36 @@
 # otherwise, increment __nb_objects and assign the new value to the public instance attribute id
 # This class will be the “base” of all other classes in this project. The goal of it is to manage id attribute in all your future classes and to avoid duplicating the same code (by extension, same bugs)
 
+
+"""
+Module: base
+
+This module defines the Base class, which serves as the base class for other classes in the project.
+"""
+
 class Base:
+    """
+    Class Base serves as the base class for other classes in the project.
+
+    Attributes:
+        __nb_objects (int): A private class attribute to manage the id attribute in all future classes.
+    """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
+        """
+        Initializes a new instance of the Base class.
+
+        Args:
+            id (int, optional): The id for the instance. If provided, it will be assigned to the id attribute.
+                If not provided, the __nb_objects counter will be incremented and assigned as the id.
+
+        Note:
+            You can assume that id is an integer.
+        """
         if id is not None:
-            id = any
+            self.id = id
         else:
             Base.__nb_objects += 1
+            self.id = Base.__nb_objects
