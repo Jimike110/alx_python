395afb59d52bbb2420fa52fff81e77db48865b43
Jimike <oladoyemike@gmail.com>
2023-10-01 04:04:08 +0100

    Task 6. Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -2,6 +2,7 @@
 # Instantiation with width and height: def __init__(self, width, height):
 #     width and height must be private. No getter or setter
 #     width and height must be positive integers, validated by integer_validator
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 """
 BaseGeometry class
@@ -22,31 +23,6 @@ class TypeMetaClass(type):
         return [
             attribute for attribute in attributes if attribute != "__init_subclass__"
         ]
-
-
-class BaseGeometry(metaclass=TypeMetaClass):
-    """
-    This is a base class
-    """
-
-    def __dir__(self):
-        """
-        Exclude attribute init subclass in dir()
-        """
-        attributes = super().__dir__()
-
-        return [
-            attribute for attribute in attributes if attribute != "__init_subclass__"
-        ]
-
-    def area(self):
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        elif value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
         
 class Rectangle(BaseGeometry):
     """
