4196f85bc6a64c2587cac42763c3b3a94f1e393c
Jimike <oladoyemike@gmail.com>
2023-10-01 03:30:40 +0100

    5. Integer validator

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -47,6 +47,6 @@ class BaseGeometry(metaclass=TypeMetaClass):
     
     def integer_validator(self, name, value):
         if value is not int:
-            raise TypeError(name, "must be an integer")
+            raise TypeError("<name> must be an integer")
         elif value <= 0:
-            raise ValueError(name, "must be greater than 0")
+            raise ValueError("<name> must be greater than 0")
