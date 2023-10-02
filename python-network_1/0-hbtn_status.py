c4d10ae2f943edb44261c63af5ed420a3d9b0a8c
Jimike <oladoyemike@gmail.com>
2023-10-02 10:09:17 +0100

    Added module documentation

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+0-hbtn_status.py - A script to fetch and display the status of https://alu-intranet.hbtn.io
+"""
+
 import requests
 
 if __name__ == "__main__":
+    # URL to fetch
     url = "https://alu-intranet.hbtn.io/status"
+
+    # Send GET request to the URL
     response = requests.get(url)
 
+    # Display information about the response
     print("Body response:")
     print("\t- type:", type(response.text))
     print("\t- content:", response.text)
